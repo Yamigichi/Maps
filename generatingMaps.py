c84f8731edcfe7cbf321affded696879a744ce80
Julian-felipe C Gonzales <54486338+Yamigichi@users.noreply.github.com>
2021-03-24 18:35:44 -0400

    Update generatingMaps.py

--- generatingMaps.py
+++ generatingMaps.py
@@ -4,3 +4,7 @@ import pandas as pd # primary data structure library
 !conda install -c conda-forge folium=0.5.0 --yes
 import folium
 #print('Folium installed and imported!')
+# define the world map
+world_map = folium.Map()
+# display world map
+world_map
