45aa2a8ff0971bddc600e3c8f479695d35854fca
Julian-felipe C Gonzales <54486338+Yamigichi@users.noreply.github.com>
2021-03-24 18:34:41 -0400

    Create generatingMaps.py

---
+++ generatingMaps.py
@@ -0,0 +1,6 @@
+import numpy as np  # useful for many scientific computing in Python
+import pandas as pd # primary data structure library
+#install folium
+!conda install -c conda-forge folium=0.5.0 --yes
+import folium
+#print('Folium installed and imported!')
